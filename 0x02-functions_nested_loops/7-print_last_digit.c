0d1afe5728a2594cbbf236a816c4dd1be27d9fbc
Michelle285 <isokenodigie285@gmail.com>
2023-07-18 17:04:09 -0700

    tr

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -15,8 +15,8 @@ int print_last_digit(int n)
 
 	if (n < 0)
 	{
-		n = n * -1;
-		last_digit = n % 10;
+		n1 = n * -1;
+		last_digit = n1 % 10;
 		_putchar(last_digit + '0');
 		return (last_digit);
 	}
