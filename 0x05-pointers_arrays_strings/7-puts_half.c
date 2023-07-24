2455803a4b3306802f0980ee75a1a545950d0b8c
Michelle285 <isokenodigie285@gmail.com>
2023-07-24 15:59:32 -0700

    tyft

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
@@ -7,21 +8,29 @@
  */
 void puts_half(char *str)
 {
-	int count = 0, i;
+	int count = 0, i, j;
 
 	while (count >= 0)
 	{
 		if (str[count] == '\0')
 			break;
+		printf("count[%d]: %c\n", count, str[count]);
 		count++;
+		
 	}
+	printf("count: %d\n", count);
 
-	if (count % 2 == 1)
+	if (count % 2 == 0)
+	{
 		i = count / 2;
+		printf("i: %d\n", i);
+	}
 	else
+	{
 		i = (count - 1) / 2;
-
-	for (i++; i < count; i++)
-		_putchar(str[i]);
+		printf("i: %d\n", i);
+	}
+	for (j = i; j < count; j++)
+		_putchar(str[j]);
 	_putchar('\n');
 }
