a789853b0792a1007af8a6b72ceb0c9f1f6d60db
Michelle285 <isokenodigie285@gmail.com>
2023-07-24 14:42:34 -0700

    trdrd

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * puts2 - prints one char out of 2 of a string.
+ * @str: input string.
+ * Return: no return.
+ */
+
+void puts2(char *str)
+{
+	int count = 0;
+
+	while (count >= 0)
+	{
+		if (str[count] != '\0')
+		{
+			_putchar(str[count]);
+			count = count + 2;
+		}
+		else
+			break;
+	}
+	_putchar('\n');
+}
