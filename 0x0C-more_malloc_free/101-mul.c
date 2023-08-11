eb306cc0f274093972b3b7bf24c9d8dfdf5e20f2
Michelle Odigie <mitchyodigie1@gmail.com>
2023-08-11 22:30:31 +0100

    tydtyf

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -10,19 +10,28 @@
  */
 int main(int argc, char **argv)
 {
-	int num1, num2, mul;
+	int num1, num2, mul, len = 0, n;
 
 	if (argc != 3)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	else
+	for (n = 1; n < argc; n++)
 	{
-		num1 = atoi(argv[1]);
-		num2 = atoi(argv[2]);
-		mul = num1 * num2;
-		printf("%d\n", mul);
-		return (0);
+		while (argv[n][len] != '\0')
+		{
+			if (!isdigit(argv[n][len]))
+			{
+				printf("Error\n");
+				exit(98);
+			}
+			len++;
+		}
 	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	mul = num1 * num2;
+	printf("%d\n", mul);
+	return (0);
 }
