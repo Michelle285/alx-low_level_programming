1257b544f4e8013a103d5999bc31df11242ad174
Michelle Odigie <mitchyodigie1@gmail.com>
2023-08-11 20:57:22 +0100

    gfgh

---
+++ 0x0C-more_malloc_free/test.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - check code
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+    printf("Hello world\n");
+    printf("It's Michelle\n");
+    printf("Nice to meet you!!\n");
+    return (0);
+}
+
