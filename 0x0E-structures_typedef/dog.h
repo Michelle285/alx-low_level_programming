2d11cef92f5ca0f238c25cf4983b771c59d08f5c
Michelle Odigie <mitchyodigie1@gmail.com>
2023-08-14 23:06:49 +0100

    tfhh

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#define _DOG_H_
+
+/**
+ * struct dog - struct for a dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ * Description: struct that stores some information
+ * about a dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
