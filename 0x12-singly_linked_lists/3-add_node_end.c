2066abedb526aaac8f10383596417436f14db0e0
Michelle Odigie <mitchyodigie1@gmail.com>
2023-08-25 01:24:57 +0100

    ygf

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,5 +1,4 @@
 #include "lists.h"
-#include <stdio.h>
 
 /**
  * add_node_end - adds a new node at the
@@ -8,33 +7,41 @@
  * @str: pointer to string
  * Return: address of the new element
  */
-
 list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new, *temp;
 	size_t len;
 
 	new = malloc(sizeof(list_t));
-	if (!new)
+	if (new == NULL)
 		return (NULL);
+
+	new->str = strdup(str);
+
+	if (str == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+
 
 	while (str[len])
 		len++;
 
-	new->str = strdup(str);
 	new->len = len;
-	if (*head == NULL)
+	new->next = NULL;
+	temp = *head;
+
+	if (temp == NULL)
 	{
-		new->next = *head;
 		*head = new;
 	}
 	else
 	{
-		new->next = NULL;
-		temp = *head;
-		while (temp->next)
+		while (temp->next != NULL)
 			temp = temp->next;
 		temp->next = new;
 	}
-	return (new);
+
+	return (*head);
 }
